245e18464576dd5cd657d713a271f34e60f0a218
eyecandycode <56252980+eyecandycode@users.noreply.github.com>
2020-01-31 14:04:07 +0000

    Clean up backend app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-// var cookieParser = require("cookie-parser");
-// var logger = require("morgan");
 var connectDB = require("./config/db");
 require("dotenv").config();
 
@@ -13,39 +10,13 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-
-// // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
-
-// app.use(logger("dev"));
-// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
 
 //Define Routes
-// app.use("/", require("./routes/index"));
 app.use("/fonts", require("./routes/fonts"));
 app.use("/users", require("./routes/users"));
 app.use("/auth", require("./routes/auth"));
 app.use("/favs", require("./routes/favs"));
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -61,5 +32,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
-
-// module.exports = app;
